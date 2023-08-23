4e9ce42452829b0397c9c83d6194ce354850e3f6
Qi Liu <evelyn.liuqi@gmail.com>
2023-08-23 22:30:52 +0800

    wip

--- packages/puppeteer-core/src/injected/util.ts
+++ packages/puppeteer-core/src/injected/util.ts
@@ -27,7 +27,7 @@ export const checkVisibility = (
 
 function isBoundingBoxEmpty(element: Element): boolean {
   const rect = element.getBoundingClientRect();
-  return !!(rect.width || rect.height || rect.top || rect.bottom);
+  return !(rect.width || rect.height || rect.top || rect.bottom);
 }
 
 const hasShadowRoot = (node: Node): node is Node & {shadowRoot: ShadowRoot} => {
